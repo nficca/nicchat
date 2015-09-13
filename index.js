a38b55fa5a6cc9511fcc460074676a4ce309e2f5
Nic Ficca <nicficca@gmail.com>
2015-09-13 19:06:05 -0400

    setup to run solely on node (rather than apache)

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io')(server);
 
-app.get('/', function(req, res) {
+app.use(express.static(__dirname+'/public'));
+
+app.get('/nicchat', function(req, res) {
     res.sendFile(__dirname+'/chat.html');
 });
 
@@ -18,6 +22,6 @@ io.on('connection', function(socket) {
     })
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+server.listen(80, function() {
+    console.log('listening on *:80');
 });
