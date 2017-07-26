a0750fe4a45ecc64f9e121f953ff3138df84fb01
nic <nicficca@gmail.com>
2017-07-25 21:56:49 -0400

    Don't run on port 80
    - Also, clean up index.js styling a bit

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ var moment = require('moment');
 // server
 var app = express();
 var http = require('http');
-var server = http.createServer(app);
+var server = http.Server(app);
 var io = require('socket.io')(server);
+var PORT = 9000;
 
 // initializing swig
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
-app.set('views', __dirname+'/views');
+app.set('views', __dirname + '/views');
 
 // static includes
-app.use(express.static(__dirname+'/public'));
-
+app.use(express.static(__dirname + '/public'));
 
 /* ***********************************************
 
                      ROUTING
 
 *********************************************** */
-app.get('/nicchat', function(req, res) {
-    res.render('chat/chat', {title: 'nicchat'});
+app.get('/chat', function(req, res) {
+    res.render('chat/chat', { title: 'nicchat' });
 });
 
 app.get('*', function(req,res) {
-    res.status(404).render('error/404', {title:'Not Found'});
+    res.status(404).render('error/404', { title:'Not Found' });
 });
 // ***********************************************
 
@@ -36,11 +36,11 @@ app.get('*', function(req,res) {
 // Socket.io handling for nicchat
 io.on('connection', function(socket) {
     console.log('user connected');
-    io.emit('message', {sender:'console', type:'server', text:'user connected'});
+    io.emit('message', { sender:'console', type:'server', text:'user connected' });
 
     socket.on('disconnect', function() {
         console.log('user disconnected');
-        io.emit('message', {sender:'console', type:'server', text:'user disconnected'});
+        io.emit('message', { sender:'console', type:'server', text:'user disconnected' });
     });
 
     socket.on('message', function(msgData) {
@@ -51,6 +51,6 @@ io.on('connection', function(socket) {
 
 
 // start server
-server.listen(80, function() {
-    console.log('Server listening on port 80');
+server.listen(PORT, function() {
+    console.log('Server listening on port ' + PORT);
 });
